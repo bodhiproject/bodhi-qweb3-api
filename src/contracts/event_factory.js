e3446c6091511f254a6f91f993baf2897cbaa10a
Chris Li <chris.li.2046@gmail.com>
2017-12-18 00:43:27 -0800

    Updated EventFactory.createTopic() to use chunkString to break down eventName

--- src/contracts/event_factory.js
+++ src/contracts/event_factory.js
@@ -1,8 +1,15 @@
 import Config from '../../config/config';
 import Contracts from '../../config/contracts';
+import utils from '../modules/qweb3/src/utils';
 
 const Qweb3 = require('../modules/qweb3/index');
 const qweb3 = new Qweb3(Config.QTUM_RPC_ADDRESS);
+
+/** @type {number} Length of string of eventName */
+const EVENTNAME_STR_LENGTH = 32 * 2; // 32 bytes times 2 hex character
+
+/** @type {number} Max capacity of eventName array */
+const EVENTNAME_ARRAY_CAPACITY = 10;
 
 export async function createTopic(args) {
   const { 
@@ -25,40 +32,8 @@ export async function createTopic(args) {
     return;
   }
 
-  // Break eventName into string arrays with max length of 64
-  let eventNameStringArray = [];
-  let maxArrayLength = 10;
-  let arrayCapacity = 32 * 2; // 32 bytes times 2 hex character
-
-  let currentIdx = 0;
-  let lastIdx = eventName.length - 1;
-
-  while (currentIdx < lastIdx) {
-    console.log("currentIdx is " + currentIdx);
-    let subStr;
-    let endIdx;
-
-    if (currentIdx + arrayCapacity <= lastIdx) {
-      endIdx = currentIdx + arrayCapacity;
-      subStr = eventName.slice(currentIdx, endIdx);
-    } else {
-      endIdx = lastIdx;
-      subStr = eventName.slice(currentIdx);
-    }
-
-    // _.padEnd(subStr, arrayCapacity, '0');
-
-    currentIdx = endIdx;
-
-    // Push hex string to name array.
-    // Break the loop if max array length is reached
-    if (eventNameStringArray.length < maxArrayLength) {
-      eventNameStringArray.push('0x' + subStr);
-    } else {
-      console.log("Name array reach max length " + maxArrayLength);
-      break;
-    }
-  }
+  // Break eventName into 10 strings of 64 chars
+  let eventNameStringArray = utils.chunkString(eventName, EVENTNAME_STR_LENGTH).slice(0, EVENTNAME_ARRAY_CAPACITY);
 
   const eventFactory = new qweb3.Contract(Contracts.EventFactory.address, Contracts.EventFactory.abi);
   return await eventFactory.send('createTopic', {
