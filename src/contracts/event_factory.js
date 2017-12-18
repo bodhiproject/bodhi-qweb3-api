80971c639def47bf4b8ce242bf4b5632695f548c
Chris Li <chris.li.2046@gmail.com>
2017-12-17 19:58:36 -0800

    Updated createTopic to convert string to byte32[10]

--- src/contracts/event_factory.js
+++ src/contracts/event_factory.js
@@ -25,9 +25,44 @@ export async function createTopic(args) {
     return;
   }
 
+  // Break eventName into string arrays with max length of 64
+  let eventNameStringArray = [];
+  let maxArrayLength = 10;
+  let arrayCapacity = 32 * 2; // 32 bytes times 2 hex character
+
+  let currentIdx = 0;
+  let lastIdx = eventName.length - 1;
+
+  while (currentIdx < lastIdx) {
+    console.log("currentIdx is " + currentIdx);
+    let subStr;
+    let endIdx;
+
+    if (currentIdx + arrayCapacity <= lastIdx) {
+      endIdx = currentIdx + arrayCapacity;
+      subStr = eventName.slice(currentIdx, endIdx);
+    } else {
+      endIdx = lastIdx;
+      subStr = eventName.slice(currentIdx);
+    }
+
+    // _.padEnd(subStr, arrayCapacity, '0');
+
+    currentIdx = endIdx;
+
+    // Push hex string to name array.
+    // Break the loop if max array length is reached
+    if (eventNameStringArray.length < maxArrayLength) {
+      eventNameStringArray.push('0x' + subStr);
+    } else {
+      console.log("Name array reach max length " + maxArrayLength);
+      break;
+    }
+  }
+
   const eventFactory = new qweb3.Contract(Contracts.EventFactory.address, Contracts.EventFactory.abi);
   return await eventFactory.send('createTopic', {
-    methodArgs: [oracleAddress, eventName, resultNames, bettingEndBlock, resultSettingEndBlock],
+    methodArgs: [oracleAddress, eventNameStringArray, resultNames, bettingEndBlock, resultSettingEndBlock],
     gasLimit: 5000000,
     senderAddress: senderAddress,
   });
