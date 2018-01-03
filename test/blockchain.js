dddac1aea40d94a60accedf2ed5d01366a4a279f
Deric Walintukan <deric.walintukan@gmail.com>
2018-01-03 17:12:22 +0700

    Add all needed dependencies and add first blockchain test

--- test/blockchain.js
+++ test/blockchain.js
@@ -1,12 +1,15 @@
+import 'babel-polyfill';
+import { assert } from 'chai';
+
 import Blockchain from '../src/blockchain.js';
 
 describe('Blockchain', function() {
 
-  describe('getBlockCount()', function() {
+  describe('getBlockCount()', async function() {
     it('returns the blockcount', async function() {
       const res = await Blockchain.getBlockCount();
       assert.isDefined(res);
       assert.isNumber(res);
     });
   });
-}
+});
