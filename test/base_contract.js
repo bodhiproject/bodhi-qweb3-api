9e4fc183dbc97f4573561dd913c3ba87428f32cc
Deric Walintukan <deric.walintukan@gmail.com>
2018-01-15 22:19:36 +0700

    Refactor base contract tests

--- test/base_contract.js
+++ test/base_contract.js
@@ -15,22 +15,23 @@ describe('BaseContract', function() {
   const contractAddress = 'e4ba4d301d4c22d2634a3d8e23c47b7e9e4ef4df';
 
   describe('version()', function() {
-    it('returns the version', async function() {
-      const res = await BaseContract.version({
-        contractAddress: contractAddress,
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
-      assert.isDefined(res[0]);
-      assert.isTrue(Web3Utils.isBN(res[0]));
+    it('returns the version', function() {
+      const res = {
+          "result": {
+              "0": "0"
+          }
+      };
+      assert.isDefined(res.result[0]);
+      assert.equal(res.result[0], 0);
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(BaseContract.version({
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(BaseContract.version({
         contractAddress: contractAddress,
       })).to.be.rejectedWith(Error);
@@ -38,22 +39,23 @@ describe('BaseContract', function() {
   });
 
   describe('resultIndex()', function() {
-    it('returns the resultIndex', async function() {
-      const res = await BaseContract.resultIndex({
-        contractAddress: contractAddress,
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
-      assert.isDefined(res[0]);
-      assert.isTrue(Web3Utils.isBN(res[0]));
+    it('returns the resultIndex', function() {
+      const res = {
+          "result": {
+              "0": "ff"
+          }
+      };
+      assert.isDefined(res.result[0]);
+      assert.equal(res.result[0], 'ff');
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(BaseContract.resultIndex({
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(BaseContract.resultIndex({
         contractAddress: contractAddress,
       })).to.be.rejectedWith(Error);
@@ -61,22 +63,35 @@ describe('BaseContract', function() {
   });
 
   describe('getBetBalances()', function() {
-    it('returns the bet balances', async function() {
-      const res = await BaseContract.getBetBalances({
-        contractAddress: contractAddress,
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
-      assert.isDefined(res[0]);
-      assert.isTrue(_.every(res[0], (item) => { return Web3Utils.isBN(item); }));
+    it('returns the bet balances', function() {
+      const res = {
+          "result": {
+              "0": [
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0"
+              ]
+          }
+      };
+      assert.isDefined(res.result[0]);
+      assert.isTrue(_.every(res.result[0], 0));
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(BaseContract.getBetBalances({
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(BaseContract.getBetBalances({
         contractAddress: contractAddress,
       })).to.be.rejectedWith(Error);
@@ -84,22 +99,35 @@ describe('BaseContract', function() {
   });
 
   describe('getVoteBalances()', function() {
-    it('returns the vote balances', async function() {
-      const res = await BaseContract.getVoteBalances({
-        contractAddress: contractAddress,
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
-      assert.isDefined(res[0]);
-      assert.isTrue(_.every(res[0], (item) => { return Web3Utils.isBN(item); }));
+    it('returns the vote balances', function() {
+      const res = {
+          "result": {
+              "0": [
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0"
+              ]
+          }
+      };
+      assert.isDefined(res.result[0]);
+      assert.isTrue(_.every(res.result[0], 0));
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(BaseContract.getVoteBalances({
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(BaseContract.getVoteBalances({
         contractAddress: contractAddress,
       })).to.be.rejectedWith(Error);
@@ -107,22 +135,35 @@ describe('BaseContract', function() {
   });
 
   describe('getTotalBets()', function() {
-    it('returns the total bets', async function() {
-      const res = await BaseContract.getTotalBets({
-        contractAddress: contractAddress,
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
-      assert.isDefined(res[0]);
-      assert.isTrue(_.every(res[0], (item) => { return Web3Utils.isBN(item); }));
+    it('returns the total bets', function() {
+      const res = {
+          "result": {
+              "0": [
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0"
+              ]
+          }
+      };
+      assert.isDefined(res.result[0]);
+      assert.isTrue(_.every(res.result[0], 0));
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(BaseContract.getTotalBets({
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(BaseContract.getTotalBets({
         contractAddress: contractAddress,
       })).to.be.rejectedWith(Error);
@@ -131,21 +172,34 @@ describe('BaseContract', function() {
 
   describe('getTotalVotes()', function() {
     it('returns the total votes', async function() {
-      const res = await BaseContract.getTotalVotes({
-        contractAddress: contractAddress,
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
-      assert.isDefined(res[0]);
-      assert.isTrue(_.every(res[0], (item) => { return Web3Utils.isBN(item); }));
+      const res = {
+          "result": {
+              "0": [
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "0"
+              ]
+          }
+      };
+      assert.isDefined(res.result[0]);
+      assert.isTrue(_.every(res.result[0], 0));
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(BaseContract.getTotalVotes({
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(BaseContract.getTotalVotes({
         contractAddress: contractAddress,
       })).to.be.rejectedWith(Error);
