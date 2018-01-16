d5a7ef08f6bc3cb55012be7ca5f52f23e0c4a848
Deric Walintukan <deric.walintukan@gmail.com>
2018-01-16 13:59:57 +0700

    Refactor centralized oracle with mocks

--- test/centralized_oracle.js
+++ test/centralized_oracle.js
@@ -2,10 +2,12 @@ import 'babel-polyfill';
 import Chai from 'chai';
 import ChaiAsPromised from 'chai-as-promised';
 import Web3Utils from 'web3-utils';
+import { Decoder } from 'qweb3';
 
 import CentralizedOracle from '../src/centralized_oracle.js';
 import ContractUtils from './util/contract_utils';
 import TestConfig from './config/test_config';
+import Mocks from './mocks';
 
 Chai.use(ChaiAsPromised);
 const assert = Chai.assert;
@@ -15,17 +17,12 @@ describe('CentralizedOracle', function() {
   const address = 'd78f96ea55ad0c8a283b6d759f39cda34a7c5b10';
 
   describe('bet()', function() {
-    it('returns a tx receipt', async function() {
-      const res = await CentralizedOracle.bet({
-        contractAddress: address,
-        index: 1,
-        amount: 1,
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
+    it('returns a tx receipt', function() {
+      const res = Mocks.bet.result;
       assert.isTrue(ContractUtils.isTxReceipt(res));
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(CentralizedOracle.bet({
         index: 1,
         amount: 1,
@@ -33,7 +30,7 @@ describe('CentralizedOracle', function() {
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if index is undefined', async function() {
+    it('throws if index is undefined', function() {
       expect(CentralizedOracle.bet({
         contractAddress: address,
         amount: 1,
@@ -41,7 +38,7 @@ describe('CentralizedOracle', function() {
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if amount is undefined', async function() {
+    it('throws if amount is undefined', function() {
       expect(CentralizedOracle.bet({
         contractAddress: address,
         index: 1,
@@ -49,7 +46,7 @@ describe('CentralizedOracle', function() {
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(CentralizedOracle.bet({
         contractAddress: address,
         index: 1,
@@ -59,30 +56,26 @@ describe('CentralizedOracle', function() {
   });
 
   describe('setResult()', function() {
-    it('returns a tx receipt', async function() {
-      const res = await CentralizedOracle.setResult({
-        contractAddress: address,
-        resultIndex: 1,
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
+    it('returns a tx receipt', function() {
+      const res = Mocks.setResult.result;
       assert.isTrue(ContractUtils.isTxReceipt(res));
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(CentralizedOracle.setResult({
         resultIndex: 1,
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if resultIndex is undefined', async function() {
+    it('throws if resultIndex is undefined', function() {
       expect(CentralizedOracle.setResult({
         contractAddress: address,
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(CentralizedOracle.setResult({
         contractAddress: address,
         resultIndex: 1,
@@ -91,22 +84,19 @@ describe('CentralizedOracle', function() {
   });
 
   describe('oracle()', function() {
-    it('returns the oracle', async function() {
-      const res = await CentralizedOracle.oracle({
-        contractAddress: address,
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
+    it('returns the oracle', function() {
+      const res = Mocks.oracle.result;
       assert.isDefined(res[0]);
-      assert.equal(res[0], '17e7888aa7412a735f336d2f6d784caefabb6fa3');
+      assert.isTrue(Decoder.toQtumAddress(res[0]).startsWith('q'));
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(CentralizedOracle.oracle({
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(CentralizedOracle.oracle({
         contractAddress: address,
       })).to.be.rejectedWith(Error);
@@ -114,22 +104,19 @@ describe('CentralizedOracle', function() {
   });
 
   describe('bettingStartBlock()', function() {
-    it('returns the bettingStartBlock', async function() {
-      const res = await CentralizedOracle.bettingStartBlock({
-        contractAddress: address,
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
+    it('returns the bettingStartBlock', function() {
+      const res = Mocks.bettingStartBlock.result;
       assert.isDefined(res[0]);
-      assert.isTrue(Web3Utils.isBN(res[0]));
+      assert.isTrue(Web3Utils.isHex(res[0]));
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(CentralizedOracle.bettingStartBlock({
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(CentralizedOracle.bettingStartBlock({
         contractAddress: address,
       })).to.be.rejectedWith(Error);
@@ -137,22 +124,19 @@ describe('CentralizedOracle', function() {
   });
 
   describe('bettingEndBlock()', function() {
-    it('returns the bettingEndBlock', async function() {
-      const res = await CentralizedOracle.bettingEndBlock({
-        contractAddress: address,
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
+    it('returns the bettingEndBlock', function() {
+      const res = Mocks.bettingEndBlock.result;
       assert.isDefined(res[0]);
-      assert.isTrue(Web3Utils.isBN(res[0]));
+      assert.isTrue(Web3Utils.isHex(res[0]));
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(CentralizedOracle.bettingEndBlock({
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(CentralizedOracle.bettingEndBlock({
         contractAddress: address,
       })).to.be.rejectedWith(Error);
@@ -160,22 +144,19 @@ describe('CentralizedOracle', function() {
   });
 
   describe('resultSettingStartBlock()', function() {
-    it('returns the resultSettingStartBlock', async function() {
-      const res = await CentralizedOracle.resultSettingStartBlock({
-        contractAddress: address,
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
+    it('returns the resultSettingStartBlock', function() {
+      const res = Mocks.resultSettingStartBlock.result;
       assert.isDefined(res[0]);
-      assert.isTrue(Web3Utils.isBN(res[0]));
+      assert.isTrue(Web3Utils.isHex(res[0]));
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(CentralizedOracle.resultSettingStartBlock({
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(CentralizedOracle.resultSettingStartBlock({
         contractAddress: address,
       })).to.be.rejectedWith(Error);
@@ -183,22 +164,19 @@ describe('CentralizedOracle', function() {
   });
 
   describe('resultSettingEndBlock()', function() {
-    it('returns the resultSettingEndBlock', async function() {
-      const res = await CentralizedOracle.resultSettingEndBlock({
-        contractAddress: address,
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
+    it('returns the resultSettingEndBlock', function() {
+      const res = Mocks.resultSettingEndBlock.result;
       assert.isDefined(res[0]);
-      assert.isTrue(Web3Utils.isBN(res[0]));
+      assert.isTrue(Web3Utils.isHex(res[0]));
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(CentralizedOracle.resultSettingEndBlock({
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(CentralizedOracle.resultSettingEndBlock({
         contractAddress: address,
       })).to.be.rejectedWith(Error);
