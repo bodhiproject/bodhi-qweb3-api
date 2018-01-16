4ac02cdf57227fc81d95248992b5a37a64f15c50
Deric Walintukan <deric.walintukan@gmail.com>
2018-01-16 14:11:51 +0700

    Refator event factory

--- test/event_factory.js
+++ test/event_factory.js
@@ -5,6 +5,7 @@ import ChaiAsPromised from 'chai-as-promised';
 import EventFactory from '../src/event_factory';
 import ContractUtils from './util/contract_utils';
 import TestConfig from './config/test_config';
+import Mocks from './mocks';
 
 Chai.use(ChaiAsPromised);
 const assert = Chai.assert;
@@ -13,16 +14,7 @@ describe('EventFactory', function() {
 
   describe('createTopic()', async function() {
     it('returns a tx receipt', async function() {
-      const res = await EventFactory.createTopic({
-        oracleAddress: 'qKjn4fStBaAtwGiwueJf9qFxgpbAvf1xAy',
-        eventName: 'The quick brown fox jumped over the lazy dog?',
-        resultNames: ['yes', 'no'],
-        bettingStartBlock: 65815,
-        bettingEndBlock: 65835,
-        resultSettingStartBlock: 65836,
-        resultSettingEndBlock: 66000,
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
+      const res = Mocks.createTopic.result;
       assert.isTrue(ContractUtils.isTxReceipt(res));
     });
   });
