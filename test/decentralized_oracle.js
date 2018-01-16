9e1ef0a3d0c41ac92d74918798d2cd4526d0aa8e
Deric Walintukan <deric.walintukan@gmail.com>
2018-01-16 14:04:45 +0700

    Refactor decentralized tests with mocks

--- test/decentralized_oracle.js
+++ test/decentralized_oracle.js
@@ -6,6 +6,7 @@ import Web3Utils from 'web3-utils';
 import DecentralizedOracle from '../src/decentralized_oracle';
 import ContractUtils from './util/contract_utils';
 import TestConfig from './config/test_config';
+import Mocks from './mocks';
 
 Chai.use(ChaiAsPromised);
 const assert = Chai.assert;
@@ -14,17 +15,12 @@ const expect = Chai.expect;
 describe('DecentralizedOracle', function() {
 
   describe('vote()', function() {
-    it('returns a tx receipt', async function() {
-      const res = await DecentralizedOracle.vote({
-        contractAddress: 'e5b0676c6445e6d82b39e8c2a6f7e338bd0a577e',
-        resultIndex: 1,
-        botAmount: '5F5E100',
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
+    it('returns a tx receipt', function() {
+      const res = Mocks.vote.result;
       assert.isTrue(ContractUtils.isTxReceipt(res));
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(DecentralizedOracle.vote({
         resultIndex: 1,
         botAmount: '5F5E100',
@@ -32,7 +28,7 @@ describe('DecentralizedOracle', function() {
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if resultIndex is undefined', async function() {
+    it('throws if resultIndex is undefined', function() {
       expect(DecentralizedOracle.vote({
         contractAddress: 'e5b0676c6445e6d82b39e8c2a6f7e338bd0a577e',
         botAmount: '5F5E100',
@@ -40,7 +36,7 @@ describe('DecentralizedOracle', function() {
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if botAmount is undefined', async function() {
+    it('throws if botAmount is undefined', function() {
       expect(DecentralizedOracle.vote({
         contractAddress: 'e5b0676c6445e6d82b39e8c2a6f7e338bd0a577e',
         resultIndex: 1,
@@ -48,7 +44,7 @@ describe('DecentralizedOracle', function() {
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(DecentralizedOracle.vote({
         contractAddress: 'e5b0676c6445e6d82b39e8c2a6f7e338bd0a577e',
         resultIndex: 1,
@@ -58,21 +54,18 @@ describe('DecentralizedOracle', function() {
   });
 
   describe('finalizeResult()', function() {
-    it('returns a tx receipt', async function() {
-      const res = await DecentralizedOracle.finalizeResult({
-        contractAddress: 'e5b0676c6445e6d82b39e8c2a6f7e338bd0a577e',
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
+    it('returns a tx receipt', function() {
+      const res = Mocks.finalizeResult.result;
       assert.isTrue(ContractUtils.isTxReceipt(res));
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(DecentralizedOracle.finalizeResult({
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(DecentralizedOracle.finalizeResult({
         contractAddress: 'e5b0676c6445e6d82b39e8c2a6f7e338bd0a577e',
       })).to.be.rejectedWith(Error);
@@ -80,22 +73,19 @@ describe('DecentralizedOracle', function() {
   });
 
   describe('arbitrationEndBlock()', function() {
-    it('returns the arbitrationEndBlock', async function() {
-      const res = await DecentralizedOracle.arbitrationEndBlock({
-        contractAddress: '814e63497adb7eae5cc217c71d564ee437fb1973',
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
+    it('returns the arbitrationEndBlock', function() {
+      const res = Mocks.arbitrationEndBlock.result;
       assert.isDefined(res[0]);
-      assert.isTrue(Web3Utils.isBN(res[0]));
+      assert.isTrue(Web3Utils.isHex(res[0]));
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(DecentralizedOracle.arbitrationEndBlock({
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(DecentralizedOracle.arbitrationEndBlock({
         contractAddress: '814e63497adb7eae5cc217c71d564ee437fb1973',
       })).to.be.rejectedWith(Error);
@@ -103,22 +93,19 @@ describe('DecentralizedOracle', function() {
   });
 
   describe('lastResultIndex()', function() {
-    it('returns the lastResultIndex', async function() {
-      const res = await DecentralizedOracle.lastResultIndex({
-        contractAddress: '814e63497adb7eae5cc217c71d564ee437fb1973',
-        senderAddress: TestConfig.SENDER_ADDRESS,
-      });
+    it('returns the lastResultIndex', function() {
+      const res = Mocks.lastResultIndex.result;
       assert.isDefined(res[0]);
-      assert.isTrue(Web3Utils.isBN(res[0]));
+      assert.isTrue(Web3Utils.isHex(res[0]));
     });
 
-    it('throws if contractAddress is undefined', async function() {
+    it('throws if contractAddress is undefined', function() {
       expect(DecentralizedOracle.lastResultIndex({
         senderAddress: TestConfig.SENDER_ADDRESS,
       })).to.be.rejectedWith(Error);
     });
 
-    it('throws if senderAddress is undefined', async function() {
+    it('throws if senderAddress is undefined', function() {
       expect(DecentralizedOracle.lastResultIndex({
         contractAddress: '814e63497adb7eae5cc217c71d564ee437fb1973',
       })).to.be.rejectedWith(Error);
